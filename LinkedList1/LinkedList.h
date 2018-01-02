4ce9a0463418a5d8b469050a36b90c9ec873c166
Bobby Alger <thebobmister54@gmail.com>
2018-01-01 20:41:22 -0700

    Empty functions to get a first compile on linux

--- LinkedList1/LinkedList.h
+++ LinkedList1/LinkedList.h
@@ -19,7 +19,7 @@ public:
 
 	Do not allow duplicate values in the list.
 	*/
-	void insertHead(T value);
+	void insertHead(T value) {};
 
 	/*
 	insertTail
@@ -28,7 +28,7 @@ public:
 
 	Do not allow duplicate values in the list.
 	*/
-	void insertTail(T value);
+	void insertTail(T value) {};
 
 	/*
 	insertAfter
@@ -39,7 +39,7 @@ public:
 	A node should only be added if the node whose value is equal to
 	insertionNode is in the list. Do not allow duplicate values in the list.
 	*/
-	void insertAfter(T value, T insertionNode);
+	void insertAfter(T value, T insertionNode) {};
 
 	/*
 	remove
@@ -48,14 +48,14 @@ public:
 
 	The list may or may not include a node with the given value.
 	*/
-	void remove(T value);
+	void remove(T value) {};
 
 	/*
 	clear
 
 	Remove all nodes from the list.
 	*/
-	void clear();
+	void clear(){};
 
 	/*
 	at
@@ -65,14 +65,14 @@ public:
 
 	If the given index is out of range of the list, throw an out of range exception.
 	*/
-	T at(int index);
+	T at(int index) {};
 
 	/*
 	size
 
 	Returns the number of nodes in the list.
 	*/
-	int size();
+	int size() { return 0; };
 
 	/*
 	toString
@@ -83,6 +83,6 @@ public:
 	For example, a LinkedList containing the value 1, 2, 3, 4, and 5 should return
 	"1 2 3 4 5"
 	*/
-	string toString();
+	string toString() { string empty = ""; return empty; };
 };
 
